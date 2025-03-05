1f455740651eddeec523edc2c727e52da9bbde56
Noah7575 <147681307+Noah7575@users.noreply.github.com>
2025-03-05 11:54:49 +0000

    250305

---
+++ 이것이 코딩 테스트다/1차 풀이/ch13_DFS/15 풀이.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+N, M, K, X = map(int, input().split())
+
+graph = [
+    [] for _ in range(N + 1)
+]
+
+dist = [0] * (N + 1)
+
+# 그래프 정보 입력받기
+for i in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+def bfs(graph, start):
+    # 큐
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+                dist[i] = dist[v] + 1
+
+visited = [False] * (N+1)
+
+bfs(graph, X)
+
+# 결과 출력
+for i in range(N+1):
+    if dist[i] == K:
+        print(i)
+
+if K not in dist:
+    print(-1)
+
