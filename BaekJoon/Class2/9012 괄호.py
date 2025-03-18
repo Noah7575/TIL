09831447347469e1da242f0f8774be574a8d6127
Yun <dongmin7575@gmail.com>
2025-03-18 20:56:44 +0900

    240318 al

--- BaekJoon/Class2/9012 괄호.py
+++ BaekJoon/Class2/9012 괄호.py
@@ -1,49 +1,26 @@
-from collections import deque
-
 N = int(input().strip())
 
-datas = []
+data = []
 for i in range(N):
-    temp = list(input().split())
-    datas.append(temp)
+    data.append(input().strip())
 
-# VPS인지 판정하는 함수
-def VPS(data):
-    if data[0] == ')':
-        return False
-    
-    queue = deque()
-    for d in data:
-        queue.append(d)
-    
-    left = 0
-    while queue:
-        temp = queue.popleft()
-        if temp == '(':
-            left += 1
-        # 오른쪽이 나오면 짝 맞춰보기
-        if temp == ')':
-            left -= 1
-            if left == 0:
-                continue
+def VPS(datum):
+    stack = []
+    for d in datum:
+        if d == '(':
+            stack.append(d)
+        elif d == ')':
+            # 앞에 (가 있으면
+            if stack:
+                stack.pop()
             else:
-                for _ in range(left):
-                    temp2 = queue.popleft()
-                    if temp2 == ')':
-                        left -= 1
-                        if left == 0:
-                            break
-                    else:
-                        return False
-    
-    if left == 0:
-        return True
+                return 'NO'
+    # 판정이 끝나면 스택은 비어 있어야 함
+    if not stack:
+        return 'YES'
     else:
-        return False
+        return 'NO'
 
 
-for i in range(N):
-    if VPS(datas[i]):
-        print("YES")
-    else:
-        print("NO")
+for datum in data:
+    print(VPS(datum))
